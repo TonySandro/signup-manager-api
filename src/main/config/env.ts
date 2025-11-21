87ead79edbcf2a64d8659dbbd07046cced94b067
Tony Sandro <tonysduarte101@gmail.com>
2025-11-21 00:36:04 -0300

    fix: set default baseUrl value in env configuration

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
-  baseUrl: process.env.BASE_URL,
+  baseUrl:
+    process.env.BASE_URL ||
+    "https://user-manager-api-production.up.railway.app/api",
   port: process.env.PORT || 8080,
 };
--- src/utils/check-user-manager.ts
+++ src/utils/check-user-manager.ts
@@ -3,6 +3,7 @@ import env from "../main/config/env";
 
 export const checkUserManagerConnection = async (): Promise<boolean> => {
   try {
+    console.log("url: ", env.baseUrl);
     const response = await axios.get(`${env.baseUrl}/health`).catch((err) => {
       console.log("Response catch error: ", err);
       return err;
