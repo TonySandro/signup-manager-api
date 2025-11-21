798b208620a7f100cf0892153ca97b38d2532a8c
Tony Sandro <tonysduarte101@gmail.com>
2025-11-21 00:11:45 -0300

    fix: add error handling for axios request in checkUserManagerConnection

--- src/utils/check-user-manager.ts
+++ src/utils/check-user-manager.ts
@@ -3,7 +3,10 @@ import env from "../main/config/env";
 
 export const checkUserManagerConnection = async (): Promise<boolean> => {
   try {
-    const response = await axios.get(`${env.baseUrl}/health`);
+    const response = await axios.get(`${env.baseUrl}/health`).catch((err) => {
+      console.log("Response catch error: ", err);
+      return err;
+    });
 
     return response.status === 200;
   } catch {
